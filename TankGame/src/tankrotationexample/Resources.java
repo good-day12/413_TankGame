43942e48fd8cad0ed92e5a32b14320d2c13691cf
Andre Flores <floresandre91@gmail.com>
2022-11-01 22:24:59 -0700

    added Resources.java

---
+++ TankGame/src/tankrotationexample/Resources.java
@@ -0,0 +1,14 @@
+package tankrotationexample;
+
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Resources {
+
+    private static final Map<String, BufferedImage> sprites = new HashMap<>();
+
+    private static void initSprites(){
+
+    }
+}

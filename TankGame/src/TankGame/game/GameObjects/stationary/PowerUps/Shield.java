580de42ff017ac10606e1e8a8c3e5fd9a66350d4
good-day12 <106358554+good-day12@users.noreply.github.com>
2022-12-05 23:37:43 -0800

    cleaning code up, adding comments

--- TankGame/src/TankGame/game/GameObjects/stationary/PowerUps/Shield.java
+++ TankGame/src/TankGame/game/GameObjects/stationary/PowerUps/Shield.java
@@ -38,9 +38,9 @@ public class Shield extends PowerUp {
      */
     @Override
     public void collide(Tank t) {
-        Resources.getSound("powerup").playSound();
         //ensure tank doesn't already have a shield
         if (!t.isShield()) {
+            Resources.getSound("powerup").playSound();
             this.img = Resources.getSprite("shield" + t.getTankId());
             this.t = t;
             t.addShield(this);
